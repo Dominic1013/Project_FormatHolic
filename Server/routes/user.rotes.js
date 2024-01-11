6bff012ee403c0c9f54a5d77f7821ed36ee89aef
DragCk <146335758+DragCk@users.noreply.github.com>
2024-01-11 14:21:13 +0800

    Create controller handler

--- Server/routes/user.rotes.js
+++ Server/routes/user.rotes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import requestHandler from "../handlers/request.handler";
+import userController from "../controller/user.controller";
 import { body } from "express-validator";
 import { Error } from "mongoose";
 
@@ -17,5 +19,41 @@ router.post(
     .exists()
     .withMessage("password is required")
     .isLength({ min: 8 })
-    .withMessage("password minimum 8 character")
+    .withMessage("password minimum 8 character"),
+
+  body("confirmPassword")
+    .exists()
+    .withMessage("confirmPassword is required")
+    .isLength({ min: 8 })
+    .withMessage("confirmPassword minimum 8 character"),
+
+  body("displayName")
+    .exists()
+    .withMessage("displayName is required")
+    .isLength({ min: 8 })
+    .withMessage("displayName minimum 8 character"),
+
+  requestHandler.validate,
+  userController.signUp
 );
+
+router.post(
+  "/signin",
+
+  body("username")
+    .exists()
+    .withMessage("Username is require")
+    .isLength({ min: 0 })
+    .withMessage("username minimum 8 character"),
+
+  body("password")
+    .exists()
+    .withMessage("Password is require")
+    .isLength({ min: 0 })
+    .withMessage("Password minimum 8 character"),
+
+  requestHandler.validate,
+  userController.signIn
+);
+
+export default router;
